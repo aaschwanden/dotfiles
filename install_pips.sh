250854c93de0b51cc633538dd8f7d40917601940
Andy Aschwanden <andy.aschwanden@arsc.edu>
2012-07-27 19:35:14 -0800

    - added script to install all needed macports from scratch

---
+++ install_pips.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo port -v install cmake \
+py27-numpy \
+py27-matplotlib-basemap \
+git-core +bash_completion+credential_osxkeychain+doc+pcre+python27+svn \
+gdal +python27 +netcdf +hdf5 +expat +curl +geos \
+GitX \
+fftw-3 \
+proj \
+py27-pyproj \
+py27-pandas \
+qgis +grass +python27 \
+nco \
+ncview \
+py27-pip \
+swig \
+swig-python \
+mercurial \
+cdo \
+py27-cython \
+py27-ipython +parallel +pyqt4 \
+py27-scipy \
+py27-sip \
